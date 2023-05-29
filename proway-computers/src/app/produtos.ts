edbf71324d0c246d0ad2e166b99dc766cb345e95
ViniciusJrCarlos <vinicius.jcarlos@hotmail.com>
2023-05-29 01:19:49 -0300

    inserindo imagens e preços nos produtos

---
+++ proway-computers/src/app/produtos.ts
@@ -0,0 +1,25 @@
+export interface IProduto {
+
+  id: number;
+  descricao: string;
+  preco: number;
+  descricaoPreco: string;
+  imagem: string;
+
+}
+
+export const produtos = [
+
+
+  { id: 1, descricao: "CD Metallica Novo", preco: 250.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/metallica.jpg" },
+  { id: 1, descricao: "CD Korn Novo", preco: 150.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/korn.jpg" },
+  { id: 1, descricao: "CD A7X Novo", preco: 350.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/a7x.jpg" },
+  { id: 1, descricao: "CD ArchEnemy Novo", preco: 250.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/arch.jpg" },
+  { id: 1, descricao: "CD Slipknot Novo", preco: 250.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/slipknot.jpg" },
+  { id: 1, descricao: "CD Paramore Novo", preco: 250.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/paramore.jpg" },
+  { id: 1, descricao: "CD Sorrow", preco: 150.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/sorrow.jpg" },
+  { id: 1, descricao: "CD Blessthefall", preco: 100.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/bless.jpg" },
+  { id: 1, descricao: "CD The Prodigy Raro", preco: 550.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/prodigy.jpg" },
+  { id: 1, descricao: "CD DJ MARKY", preco: 1000.00, descricaoPreco: "à vista no pix", imagem: "/assets/images/djmarky.jpg" }
+
+]
